f4c2c6c6899c0bdbdf0979d0dac84061cedf60cc
Nicholas Lemay <lemaynicholas@gmail.com>
2019-12-30 20:11:35 -0500

    An example of a simple json endpoint. Wow!

---
+++ samples/JSONMicroservice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JSONMicroservice
+{
+    public class winesController : Controller
+    {
+        [HttpGet("/wines")]
+        public ActionResult Index()
+        {
+            return Ok(new [] {new {name = "Brouilly"}, new {name = "Chateau St-Jean"}});
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebApplicationHost.CreateDefaultBuilder(args);
+
+            builder.Services.AddControllers().AddNewtonsoftJson();
+
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            await app.RunAsync();
+        }
+    }
+}
